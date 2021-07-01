b459c91ed670d55e05c565a18b92e06a50484a88
Edison Rama <edisonramaa@gmail.com>
2021-07-01 16:44:14 +0300

    Initial project commit
    Base angular + spring boot installation

---
+++ src/main/java/com/fcs/streetai/StreetAiApplication.java
@@ -0,0 +1,13 @@
+package com.fcs.streetai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreetAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreetAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fcs/streetai/StreetAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fcs.streetai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreetAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
